b1323f1b2e9d4052408c4ebc65ccbbc1d3890500
ebolo <daothanhduy305@gmail.com>
2017-05-22 20:36:57 +0700

    Add message demonstration window

---
+++ src/ebolo/ui/controller/MessageWindowController.java
@@ -0,0 +1,46 @@
+package ebolo.ui.controller;
+
+import ebolo.crypt.Steganography;
+import ebolo.net.data.Message;
+import ebolo.utils.ImageUtils;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.ImageView;
+
+/**
+ * Created by ebolo on 22/05/2017.
+ */
+public class MessageWindowController {
+    private static MessageWindowController ourInstance;
+    private Message message;
+
+    @FXML
+    private ImageView imageView;
+
+    @FXML
+    private TextArea contentBox;
+
+    public static MessageWindowController getInstance() {
+        if (ourInstance == null)
+            ourInstance = new MessageWindowController();
+        return ourInstance;
+    }
+
+    private MessageWindowController() {
+    }
+
+    public void setData(Message message) {
+        this.message = message;
+        imageView.setImage(ImageUtils.imageIconToFX(message.getEncryptedImage()));
+    }
+
+    public void clear() {
+        imageView.setImage(null);
+        contentBox.setText("");
+    }
+
+    @FXML
+    private void reveal() {
+        contentBox.setText(Steganography.decrypt(message.getEncryptedImage()));
+    }
+}
--- src/ebolo/ui/controller/ReceivedMesWindowController.java
+++ src/ebolo/ui/controller/ReceivedMesWindowController.java
@@ -2,18 +2,35 @@ package ebolo.ui.controller;
 
 import ebolo.net.data.Message;
 import ebolo.net.data.ui.ReceivedMessageManager;
+import ebolo.ui.utils.UIUtils;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import javafx.util.Callback;
+
+import java.io.IOException;
 
 /**
  * Created by ebolo on 19/05/2017.
  */
 public class ReceivedMesWindowController {
     private static ReceivedMesWindowController ourInstance;
+    private Stage messageWindow;
 
     @FXML
-    private ListView<Message> receivedListView;
+    private TableView<Message> receivedMesTableView;
+
+    @FXML
+    private TableColumn<Message, Integer> idCol;
+
+    @FXML
+    private TableColumn<Message, String> timeCol;
+
+    @FXML
 
     public static ReceivedMesWindowController getInstance() {
         if (ourInstance == null)
@@ -25,17 +42,39 @@ public class ReceivedMesWindowController {
     }
 
     public void initialize() {
-        receivedListView.setItems(ReceivedMessageManager.getInstance().getReceivedMesList());
-        receivedListView.setCellFactory(param -> new ListCell<Message>(){
-            @Override
-            protected void updateItem(Message item, boolean empty) {
-                super.updateItem(item, empty);
-                if (empty) {
-                    setText(null);
-                } else {
-                    setText(item.getDate());
+        receivedMesTableView.setItems(ReceivedMessageManager.getInstance().getReceivedMesList());
+        idCol.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(
+                null, "idNumber",
+                ReceivedMessageManager
+                        .getInstance()
+                        .getReceivedMesList()
+                        .indexOf(param.getValue()) + 1
+        ));
+        timeCol.setCellValueFactory(param -> new SimpleStringProperty(
+                param.getValue().getDate())
+        );
+        receivedMesTableView.setRowFactory(param -> {
+            TableRow<Message> row = new TableRow<>();
+            row.setOnMouseClicked(event -> {
+                if (event.getClickCount() == 2 && !row.isEmpty()) {
+                    if (messageWindow == null)
+                        try {
+                            messageWindow = UIUtils.createWindow(
+                                    "Message",
+                                    "./fxml/MessageWindowFXML.fxml",
+                                    MessageWindowController.getInstance(),
+                                    500, 500
+                            );
+                            messageWindow.setOnCloseRequest(event1 ->
+                                    MessageWindowController.getInstance().clear());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    MessageWindowController.getInstance().setData(row.getItem());
+                    messageWindow.show();
                 }
-            }
+            });
+            return row;
         });
     }
 }

f56e0b17407fcdfc1003b8c19f21a6ed093416de
ebolo <daothanhduy305@gmail.com>
2017-05-25 00:29:29 +0700

    Change ErrorDisplay to general Announcement... to support more types of warnings

--- src/ebolo/ui/utils/Announcement.java
+++ src/ebolo/ui/utils/Announcement.java
@@ -6,12 +6,13 @@ import javafx.scene.control.Alert;
 /**
  * Created by ebolo on 19/05/2017.
  */
-public class ErrorDisplay {
+public class Announcement {
     private static final String textFileErrorStr = "Invalid text file";
     private static final String imageFileErrorStr = "Invalid image file";
     private static final String ipAddrErrorStr = "Invalid IP address";
 
-    public static void showError (String errorCode, String... customMessages) {
+    public static void showAnnouncement(String errorCode, Alert.AlertType alertType,
+                                        String header, String... customMessages) {
         StringBuilder errorBuilder = new StringBuilder("");
         if (errorCode.length() == 3 && Integer.parseInt(errorCode) >= 0) {
             if (Integer.parseInt("" + errorCode.toCharArray()[0]) == 1)
@@ -28,9 +29,13 @@ public class ErrorDisplay {
             }
         }
         Platform.runLater(() -> {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setTitle("Error Sending Message");
-            alert.setHeaderText("Cannot send message");
+            Alert alert = new Alert(alertType);
+            alert.setTitle(
+                    alertType == Alert.AlertType.ERROR? "Error" :
+                            alertType == Alert.AlertType.WARNING? "Warning" :
+                                    alertType == Alert.AlertType.INFORMATION? "Information" : ""
+            );
+            alert.setHeaderText(header);
             alert.setContentText(errorBuilder.toString());
             alert.showAndWait();
         });

1d107839f734959cf8f38c65befb8090fbb072a4
ebolo <daothanhduy305@gmail.com>
2017-05-31 01:44:17 +0700

    Temporary solution for the error of... opening 2nd instance of the application. By doing that, the new instance can't bind to already been busy port and raising BindException. So that the serverSocket is null and can't be closed (obviously)

--- src/ebolo/net/listeners/IncomingListener.java
+++ src/ebolo/net/listeners/IncomingListener.java
@@ -1,9 +1,14 @@
 package ebolo.net.listeners;
 
+import ebolo.Main;
 import ebolo.net.connections.IncomingConnection;
+import ebolo.ui.utils.Announcement;
 import ebolo.utils.Configurations;
+import javafx.application.Platform;
+import javafx.scene.control.Alert;
 
 import java.io.IOException;
+import java.net.BindException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -31,21 +36,41 @@ public class IncomingListener {
 
 
     public void stopListening() throws IOException {
-        serverSocket.close();
+        if (serverSocket != null)
+            serverSocket.close();
         listeningThread.interrupt();
     }
 
     private void startServer() {
         try {
             synchronized (Configurations.getInstance()) {
-                serverSocket = new ServerSocket(Configurations.getInstance().getPORT());
+                try {
+                    serverSocket = new ServerSocket(Configurations.getInstance().getPORT());
+                } catch (BindException e) {
+                    System.out.println(
+                            "Cannot bind socket to port " + Configurations.getInstance().getPORT()
+                    );
+                    Platform.runLater(() -> {
+                        Announcement.showAnnouncement(
+                                "",
+                                Alert.AlertType.ERROR,
+                                "Cannot Start Application",
+                                "Cannot bind socket to port "
+                                        + Configurations.getInstance().getPORT()
+                        );
+                        Main.getMainStage().close();
+                    });
+                }
             }
-            System.out.println("Start listening");
-            while (true) {
-                Socket peer = serverSocket.accept();
-                new Thread(new IncomingConnection(peer)).start();
+            if (serverSocket != null) {
+                System.out.println("Start listening");
+                while (true) {
+                    Socket peer = serverSocket.accept();
+                    new Thread(new IncomingConnection(peer)).start();
+                }
             }
         } catch (IOException e) {
+            e.printStackTrace();
             System.out.println("Stopped listening");
         }
     }
